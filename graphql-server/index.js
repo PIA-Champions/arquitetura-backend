2f08251b2b0a0a6e959f587f040502d74e9cac93
Leonardo Rodrigues <108198506+LRGNIT@users.noreply.github.com>
2023-10-30 23:03:14 -0300

    Update index.js
    Alteração em todo arquivo index.js

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -1,115 +1,139 @@
-import { ApolloServer, gql } from "apollo-server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServer } from "@apollo/server";
+import { response } from "express";
+import fetch from 'node-fetch';
 
 const enquetes = [
   {
-    id: "1",
-    title: "Cachorro ou gato?",
-    description: "Qual é o melhor bichinho de estimação?",
-    options: [
+    "options": [
       {
-        id: "1",
-        description: "gato",
-        votes: 0,
+        "description": "gato",
+        "votes": 0,
+        "id": "21878480-729f-11ee-8ecb-f70fa366a824"
       },
       {
-        id: "2",
-        description: "cachorro",
-        votes: 0,
-      },
+        "description": "cachorro",
+        "votes": 0,
+        "id": "21878481-729f-11ee-8ecb-f70fa366a824"
+      }
     ],
-  },
-  {
-    id: "2",
-    title: "Coca-Cola ou Pepsi?",
-    description: "Qual é o melhor refrigerante?",
-    options: [
-      {
-        id: "1",
-        description: "coca-cola",
-        votes: 0,
-      },
-      {
-        id: "2",
-        description: "pepsi",
-        votes: 0,
-      },
-    ],
+    "description": "Qual é o melhor bichinho de estimação?",
+    "id": "21878482-729f-11ee-8ecb-f70fa366a824",
+    "title": "Cachorro ou gato?"
   },
 ];
 
-const typeDefs = gql`
-  type Option {
-    id: ID
-    description: String
-    votes: Int
-  }
-
+const typeDefs = `#graphql
+  
   type Enquete {
-    id: ID
     title: String
+    id: String
     description: String
     options: [Option]
   }
 
+  type Option {
+    description: String
+    votes: Int
+    id: String
+  }
+  
   type Query {
     enquetes: [Enquete]
     options: [Option]
   }
 
-  type Mutation {
-    createEnquete(title: String, description: String, options: [String]): Enquete
-    deleteEnquete(id: ID): Enquete
-    vote(enqueteId: ID, optionId: ID): Enquete    
+  input Option2 {
+    description: String
+    votes: Int
+    id: String
   }
+
+  type Message {
+    description: String
+    id: String
+    votes: Int
+  }
+
+  type Mutation {
+    addEnquete(title: String, id: String, description: String, options: [Option2]): Enquete
+    addVote(id: String, idItem: String): Message
+    listaEspecifico(id: String): Enquete
+    deletarEnquete(id: ID): Enquete
+  }
+
 `;
 
 const resolvers = {
   Query: {
     enquetes: () => enquetes,
+    enquetes: () => findAllEnquetes(),
   },
+
+  // adicione dentro do resolvers
   Mutation: {
-    createEnquete: (_, { title, description, options }) => {
-      const id = String(enquetes.length + 1);
-      const newEnquete = {
-        id,
-        title,
-        description,
-        options: options.map((option, index) => ({
-          id: String(index + 1),
-          description: option,
-          votes: 0,
-        })),
-      };
-      enquetes.push(newEnquete);
-      return newEnquete;
+    addEnquete: (_, { title, id, description, options }) => {
+      const enquete = {title, id, description, options }
+      return createEnquete(enquete)
     },
-    vote: (_, { enqueteId, optionId }) => {
-      for (const enquete of enquetes) {
-        if (enquete.id === enqueteId) {
-          for (const option of enquete.options) {
-            if (option.id === optionId) {
-              option.votes += 1;
-              return enquetes; // Retorne a lista de enquetes atualizada.
-            }
-          }
-        }
-      }
-      throw new Error("Enquete ou opção não encontrada.");
-    },
-    deleteEnquete: (_, { id }) => {
+    deletarEnquete: (_, { id }) => {
       const index = enquetes.findIndex(enquete => enquete.id === id);
       if (index !== -1) {
-        const deletedEnquete = enquetes.splice(index, 1)[0];
-        return deletedEnquete;
+        //const deletedEnquete = enquetes.splice(index, 1)[0];
+        // return deleteEnquete(enquetes);
+        return deleteEnquete(id);
       } else {
         throw new Error("Enquete não encontrada.");
       }
     },
+    addVote: (_, { id, idItem }) => addEnqueteVote(id,idItem),
+    listaEspecifico: (_, { id }) => findEspecificEnquetes(id)  
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
 });
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+function findAllEnquetes() {
+	return fetch('https://ix6iixqdf3.execute-api.us-east-1.amazonaws.com/prd/survey', {
+		method: 'GET',
+		headers: { 'Content-Type' : 'application/json' }
+	}).then((response) => response.json())
+}
+
+function findEspecificEnquetes(id) {
+	return fetch(`https://ix6iixqdf3.execute-api.us-east-1.amazonaws.com/prd/survey/${id}`, {
+		method: 'GET',
+		headers: { 'Content-Type' : 'application/json' }
+	}).then((response) => response.json())
+}
+
+function createEnquete(enquete){
+  return fetch('https://ix6iixqdf3.execute-api.us-east-1.amazonaws.com/prd/survey', {
+    method: 'POST',
+    body: JSON.stringify(enquete),
+    headers: { 'Content-Type' : 'application/json' }
+  }).then((response) => response.json())
+}
+
+function addEnqueteVote(id,idItem) {
+	  return fetch(`https://ix6iixqdf3.execute-api.us-east-1.amazonaws.com/prd/survey/${id}/option/${idItem}/"/r/n`, {
+	  method: 'PUT',
+    headers: { 'Content-Type' : 'application/json' }
+  }).then((response) => response.json())
+}
+
+function deleteEnquete(id){
+  return fetch(`https://ix6iixqdf3.execute-api.us-east-1.amazonaws.com/prd/survey/${id}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type' : 'application/json' }
+  }).then((response) => response.json())
+}
+
+console.log(`🚀  Server ready at: ${url}`);
