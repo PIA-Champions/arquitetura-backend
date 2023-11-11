88bc44c8de9f702e57b238b2580e10e07b67a574
Larissa170 <larissa.m.rodrigues1702@gmail.com>
2023-11-10 22:32:26 -0300

    alteração votar
    Signed-off-by: Larissa170 <larissa.m.rodrigues1702@gmail.com>

--- websocket/index.js
+++ websocket/index.js
@@ -5,6 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const { GraphQLClient } = require('graphql-request');
+const { log } = require('console');
 
 // URL do seu endpoint GraphQL
 const endpoint = 'https://bgsjdeelyk.execute-api.us-east-1.amazonaws.com';
@@ -28,10 +29,10 @@ io.on('connection', (socket) => {
           }
         }
       }`;
-  
+
       const data = await client.request(query);
       console.log('Enquetes recuperadas:', data);
-  
+
       // Enviar as enquetes recuperadas para o cliente
       socket.emit('enquetes recuperadas', data.enquetes);
     } catch (error) {
@@ -57,10 +58,10 @@ io.on('connection', (socket) => {
           }
         }
       }`;
-  
+
       const data = await client.request(mutation);
       console.log('Nova enquete criada:', data);
-  
+
       // Enviar mensagem para todos os clientes sobre a nova enquete
       io.emit('enquetes atualizadas', data);
     } catch (error) {
@@ -78,10 +79,10 @@ io.on('connection', (socket) => {
           message
         }
       }`;
-  
+
       const data = await client.request(mutation);
       console.log('Enquete deletada:', data);
-  
+
       io.emit('enquete deletada', enqueteId);
     } catch (error) {
       console.error('Erro ao deletar a enquete:', error);
@@ -98,7 +99,7 @@ io.on('connection', (socket) => {
           }
         }
       }`;
-  
+
       const data = await client.request(query);
       if (data.enquetes && data.enquetes[index] && data.enquetes[index].options) {
         const opcoesDaEnquete = data.enquetes[index].options;
@@ -112,22 +113,36 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('votar', (opcaoSelecionada) => {
+  socket.on('votar', async (opcaoSelecionada) => {
+    try {
+      const query = `query {
+        enquetes {
+          title
+          description
+          options {
+            description
+            votes
+          }
+        }
+      }`;
+      const data = await client.request(query);
+      const enqueteEncontrada = data.enquetes.find(enquete => enquete.description === opcaoSelecionada);
+      console.log("aqui" + data)
+      if (enqueteEncontrada) {
+        const opcaoVotada = enqueteEncontrada.opcoes.find(opcao => opcao.description === opcaoSelecionada);
 
-    const enqueteEncontrada = enquetes.find(enquete => enquete.opcoes.some(opcao => opcao.description === opcaoSelecionada));
-  
-    if (enqueteEncontrada) {
-      const opcaoVotada = enqueteEncontrada.opcoes.find(opcao => opcao.description === opcaoSelecionada);
-      if (opcaoVotada) {
-        // Incrementar o contador de votos para a opção escolhida
-        opcaoVotada.votes++;
-  
-        // Emitir a informação atualizada de enquete para todos os clientes
-        io.emit('enquetes atualizadas', enquetes);
+        if (opcaoVotada) {
+          // Incrementar o contador de votos para a opção escolhida
+          opcaoVotada.votes++;
+          // Emitir a informação atualizada de enquete para todos os clientes
+          io.emit('enquetes atualizadas', enquetes);
+        }
       }
+    } catch (error) {
+      console.error('Erro ao buscar as opções da enquete:', error);
+      io.emit('opcoes enquete', []);
     }
   });
-  
 
 });
 
